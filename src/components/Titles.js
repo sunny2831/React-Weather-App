966be0257180b9e1de07d43f1d1663f0906fbe65
sunny2831 <sunny2831@hotmail.co.uk>
2019-07-10 22:19:11 +0100

    skeletal ui components in working order and can fetch openweather api

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Titles extends  React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Weather Search</h1>
+                <p>Find out the weather conditions. Live, wherever and whenever!</p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
